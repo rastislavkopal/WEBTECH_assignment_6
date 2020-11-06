2d56459a1eb3f0b99844da434f12fd91f829286d
rastislavkopal <rasto.kopal@gmail.com>
2020-11-06 23:27:07 +0100

    whole project

---
+++ js/myScript.js
@@ -0,0 +1,141 @@
+var amplitude = 1;
+function printChart()
+{  
+  Plotly.plot('chartDiv', [{
+    y: [],
+    mode: 'lines+markers', 
+    marker: {color: 'red', size: 10},
+    line: {width: 4},
+    type: 'scatter',
+    name: 'sin(x)'
+  }, {
+    y: [],
+    mode: 'lines+markers',
+    marker: {color: 'blue', size:10},
+    line: {width: 4},
+    type: 'scatter',
+    name: 'cos(x)'
+  }]);
+}
+
+window.addEventListener('load', function () {
+  printChart();
+})
+
+var source = new EventSource("http://vmzakova.fei.stuba.sk/sse/sse.php");
+if(typeof(EventSource) !== "undefined") {
+  source.onmessage = function(event) {
+    let obj =  JSON.parse(event.data);
+    Plotly.extendTraces('chartDiv', {
+        y: [[amplitude*obj.y1],[amplitude*obj.y2]], 
+      }, [0, 1]);
+  };
+} else {
+  document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
+}
+
+function stopChart()
+{
+  source.close();
+}
+
+function sinCheck()
+{
+  if (document.getElementById("sin").checked)
+  {
+    Plotly.restyle('chartDiv', {
+      opacity: 1,
+    }, [0]);
+  } else {
+    Plotly.restyle('chartDiv', {
+      opacity: 0,
+    }, [0]);
+  }
+}
+
+function cosCheck()
+{
+  if (document.getElementById("cos").checked)
+  {
+    Plotly.restyle('chartDiv', {
+      opacity: 1,
+    }, [1]);
+  } else {
+    Plotly.restyle('chartDiv', {
+      opacity: 0,
+    }, [1]);
+  }
+}
+
+class ChartOptions extends HTMLElement  
+{
+   constructor() {
+     super();
+     this.addEventListener('click',
+     () => {
+        this.style.color === 'red'
+        ? this.style.color = 'blue':
+         this.style.color = 'red';
+      });
+   }
+
+connectedCallback() {
+     this.style.color = 'blue';
+
+     const template = document.querySelector('template');
+     const clone = document.importNode(template.content, true);
+     this.appendChild(clone);
+   }
+  
+}
+customElements.define('chart-options', ChartOptions);
+
+
+function textAmplitude()
+{
+  if (document.getElementById("checkText").checked)
+  {
+    document.getElementById("text").style.display = "block";
+  } else {
+    document.getElementById("text").style.display = "none";
+  }
+  
+}
+
+function sliderAmplitude()
+{
+  if (document.getElementById("checkSlider").checked)
+  {
+    document.getElementById("slider").style.display = "block";
+  }else {
+    document.getElementById("slider").style.display = "none";
+  }
+}
+
+
+function sliderAmplitudeApply()
+{
+  amplitude = document.getElementById("text").value;
+  console.log("val: " + amplitude);
+}
+
+function AmplitudeApply(element)
+{
+  if (element.id == "text")
+  {
+    if (element.value <= 0 || element.value > 5)
+    {
+      alert("nah, that aint allowed -> (0;5>");
+      return;
+    }
+    amplitude = document.getElementById("text").value;
+    console.log("val: " + amplitude);
+    document.getElementById("slider").value = amplitude;
+  } else // slider changed
+  {
+    amplitude = document.getElementById("slider").value;
+    console.log("val: " + amplitude);
+    document.getElementById("text").value = amplitude;
+  }
+
+}
